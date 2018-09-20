6f3982d574b04a64cb342ca59f43addc85d851d8
jtorreguitar <jtorreguitar@itba.edu.ar>
2018-09-20 14:46:12 -0300

    copied things over from deletemovie

--- src/Controllers/MovieController.cs
+++ src/Controllers/MovieController.cs
@@ -2,11 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-<<<<<<< e7ecc348cdd47acf25f15c2b530b0b58eaa6ba56
 using System.Net;
 using System.Net.Mail;
-=======
->>>>>>> added user functionality
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Localization;
@@ -19,21 +16,18 @@ using TrainingNet.Repositories.Interfaces;
 
 namespace TrainingNet.Controllers
 {
-    
+
     [Route("[controller]")]
     public class MovieController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+
         public MovieController(IUnitOfWork unitOfWork)
         {
             this._unitOfWork = unitOfWork;
         }
-<<<<<<< e7ecc348cdd47acf25f15c2b530b0b58eaa6ba56
 
-=======
-        
         [Authorize]
->>>>>>> added user functionality
         [HttpGet("Add")]
         public IActionResult Add()
         {
@@ -66,18 +60,11 @@ namespace TrainingNet.Controllers
             }
         }
 
-        [HttpPost("EditMovie/{id}")]
-<<<<<<< 18350f78f9ab7913445fb8ce2f228f0972536045
-        public IActionResult EditMovie(MovieViewModel movie, int id)
+        [HttpPost("EditMovie/{id}")]        public IActionResult EditMovie(MovieViewModel movie, int id)
         {
             try
             {
                 if (id == 0)
-=======
-        public IActionResult EditMovie(MovieViewModel movie, int id){
-            try{
-                if(id == 0)
->>>>>>> added delete movie functionality
                     throw new NullReferenceException("The movie was not found");
                 Movie movieToBeChanged = UnitOfWork.MovieRepository.Get(id);
                 movieToBeChanged.Update(movie);
@@ -125,24 +112,28 @@ namespace TrainingNet.Controllers
 
         [HttpGet("")]
         [HttpGet("ListMovies")]
-        public IActionResult ListMovies(){
-            IEnumerable movieList = UnitOfWork.Movies.GetAll();
+        public IActionResult ListMovies()
+        {
+            var movieList = UnitOfWork.MovieRepository.GetAll().Select(movie => new MovieViewModel(movie));
             return View(movieList);
         }
 
         [HttpGet("DeleteMovie/{id}")]
-        public IActionResult DeleteMovie(int id){
+        public IActionResult DeleteMovie(int id)
+        {
             //el try catch está por las dudas porque no sé lo que pasa cuando trato
             //de eliminar/gettear una película que no existe. Recordar preguntarlo.
-            try{
-                if(id == 0)
+            try
+            {
+                if (id == 0)
                     throw new NullReferenceException("The movie was not found");
                 Movie movie = UnitOfWork.MovieRepository.Get(id);
                 UnitOfWork.MovieRepository.Remove(movie);
                 UnitOfWork.Complete();
                 return RedirectToAction("ListMovies");
             }
-            catch(NullReferenceException n){
+            catch (NullReferenceException n)
+            {
                 return NotFound();
             }
         }
