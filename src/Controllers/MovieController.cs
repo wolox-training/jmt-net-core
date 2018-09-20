0c8e2b4b2c38da77c572c18edbe6f9a3ab545cb7
jtorreguitar <jtorreguitar@itba.edu.ar>
2018-09-20 15:01:34 -0300

    this commit tree will look nice or so help me...

--- src/Controllers/MovieController.cs
+++ src/Controllers/MovieController.cs
@@ -60,7 +60,8 @@ namespace TrainingNet.Controllers
             }
         }
 
-        [HttpPost("EditMovie/{id}")]        public IActionResult EditMovie(MovieViewModel movie, int id)
+        [HttpPost("EditMovie/{id}")]        
+        public IActionResult EditMovie(MovieViewModel movie, int id)
         {
             try
             {
@@ -118,25 +119,6 @@ namespace TrainingNet.Controllers
             return View(movieList);
         }
 
-        [HttpGet("DeleteMovie/{id}")]
-        public IActionResult DeleteMovie(int id)
-        {
-            //el try catch está por las dudas porque no sé lo que pasa cuando trato
-            //de eliminar/gettear una película que no existe. Recordar preguntarlo.
-            try
-            {
-                if (id == 0)
-                    throw new NullReferenceException("The movie was not found");
-                Movie movie = UnitOfWork.MovieRepository.Get(id);
-                UnitOfWork.MovieRepository.Remove(movie);
-                UnitOfWork.Complete();
-                return RedirectToAction("ListMovies");
-            }
-            catch (NullReferenceException n)
-            {
-                return NotFound();
-            }
-        }
         private IUnitOfWork UnitOfWork
         {
             get { return this._unitOfWork; }
