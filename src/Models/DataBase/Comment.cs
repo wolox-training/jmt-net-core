d5785da18e9751a6947542d811c5edb477ff0242
jtorreguitar <jtorreguitar@itba.edu.ar>
2018-09-24 13:26:25 -0300

    users can now add comments and read them

--- src/Models/DataBase/Comment.cs
+++ src/Models/DataBase/Comment.cs
@@ -5,8 +5,13 @@ namespace TrainingNet.Models.DataBase
 {
     public class Comment
     {
+        public Comment() { }
+        public Comment(Movie movie, string content)
+        {
+            Movie = movie;
+            Content = content;
+        }
         public int Id { get; set; }
-        public string content { get; set; }
+        public string Content { get; set; }
         public virtual Movie movie { get; set; }
-    }
 }
