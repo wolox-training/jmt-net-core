5b4caac321b0b3cfe3af9345710e0cc517902dc5
jtorreguitar <jtorreguitar@itba.edu.ar>
2018-10-01 13:58:43 -0300

    nothing works

---
+++ src/Controllers/api/v1/CommentApiController.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+
+namespace Controllers.api.v1
+{
+    [Route("api/v1/[controller]")]
+    public class CommentApiController : Controller
+    {
+
+    }
+}
--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function() {
+    $('.hola-button').click(function(){ 
+        alert("HOLA");
+    });
+    var commentStr = $();
+    $.ajax {
+        url:"localhost/add",
+        data: {comment: commentStr},
+        success: function(){
+            
+        },
+        error: function(){
+
+        }
+    }
+});
